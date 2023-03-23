f31069b83e512fdc3dbd96de5871c96e99af99c8
hnklp <hynek.puta@seznam.cz>
2023-03-23 01:06:09 +0100

    Added code for rolling two standard or one special six-sided dice.

--- src/dice.py
+++ src/dice.py
@@ -1,3 +1,27 @@
+import random
+
+
 class Dice:
+
+    import random
+
     def __init__(self):
-        pass
+        self.die1 = 0
+        self.die2 = 0
+        self.die3 = 0
+
+    def std_roll(self):
+        """
+        Simulate rolling two standard six-sided dice.
+        """
+        self.die1 = random.randint(1, 6)
+        self.die2 = random.randint(1, 6)
+        return [self.die1, self.die2]
+
+    def spec_roll(self):
+        """
+        Simulate rolling one user defined six-sided dice.
+        """
+        num_lst = [2, 4, 8, 16, 32, 64]
+        self.die3 = random.choice(num_lst)
+        return self.die3
