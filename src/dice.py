e14092fc28679b5acaf0e108442a6bbf3f137dfa
Hynek Půta <hynek.puta@seznam.cz>
2023-03-23 13:37:32 +0100

    Streamlined the dice function to be more cool and lit 🔥

--- src/dice.py
+++ src/dice.py
@@ -1,24 +1,25 @@
+import random
+
+
 class Dice:
 
-    import random
-
     def __init__(self):
-        self.die1 = 0
-        self.die2 = 0
-        self.die3 = 0
+        self.throw = []
 
-    def std_roll(self):
+    def std_roll(self, num_throws: int):
         """
         Simulate rolling two standard six-sided dice.
         """
-        self.die1 = random.randint(1, 6)
-        self.die2 = random.randint(1, 6)
-        return [self.die1, self.die2]
+        self.throw = []
+        for i in range(num_throws):
+            self.throw.append(random.randint(1, 6))
+        return self.throw
 
     def spec_roll(self):
         """
         Simulate rolling one user defined six-sided dice.
         """
+        self.throw = []
         num_lst = [2, 4, 8, 16, 32, 64]
-        self.die3 = random.choice(num_lst)
-        return self.die3
+        self.throw.append(random.choice(num_lst))
+        return self.throw
